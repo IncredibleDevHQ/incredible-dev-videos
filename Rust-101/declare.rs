45c534dafb8d4f41417bc6651985fbbb71e850bb
Karthic Rao <kartronics85@gmail.com>
2021-03-04 08:57:19 +0530

    Adding the code for the second episode
    Adding the code of the second episode of hello world Rust series.

---
+++ Rust-101/declare.rs
@@ -0,0 +1,9 @@
+fn main() {
+   let company = "Incredible.dev"; //string type
+   let score = 4.5;                 // float type
+   let win = true;          // boolean type
+
+   println!("Company is:{}",company);
+   println!("Score :{}",score);
+   println!("Did they win :{}",win);
+}
