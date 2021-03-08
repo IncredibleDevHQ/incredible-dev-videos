2abdf241372267d2b45bd44c1c64ed0ffb81618e
Suvij Surya <suvijsurya76@gmail.com>
2021-03-08 16:21:51 +0530

    Create Variables.hs

---
+++ Haskell/Variables.hs
@@ -0,0 +1,10 @@
+main = do 
+   let company = "Incredible.dev" --string
+   let score = 4.5  --Int
+   let win = True --Bool
+   putStr("Company is:") 
+   print(company)
+   putStr("Score is:") 
+   print(score)
+   putStr("Did they win:") 
+   print(win)
