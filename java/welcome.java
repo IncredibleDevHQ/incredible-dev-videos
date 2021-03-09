2f7f4a0712ecf663b6ca13acf45259fe51d8337d
hemalathank-27 <56719334+hemalathank-27@users.noreply.github.com>
2021-03-09 11:34:37 +0530

    Delete java directory

--- java/welcome.java
+++
@@ -1,7 +0,0 @@
-//create a file welcome.java
-public class MyFirstJavaProgram {
-   public static void main(String []args) {
-      System.out.println("Hello World"); 
-      // prints Hello World
-   }
-}
