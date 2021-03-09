4c7bb5764c02bd9d239b85f7c7e4c3f1994faaa9
hemalathank-27 <56719334+hemalathank-27@users.noreply.github.com>
2021-03-09 11:35:20 +0530

    Create welcome.java

---
+++ Java-101/welcome.java
@@ -0,0 +1,7 @@
+//create a file welcome.java
+public class MyFirstJavaProgram {
+   public static void main(String []args) {
+      System.out.println("Hello World"); 
+      // prints Hello World
+   }
+}
