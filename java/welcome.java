d5fe5c7a29127a164c18c3e4ca8ab3fa49e25e14
hemalathank-27 <56719334+hemalathank-27@users.noreply.github.com>
2021-03-09 11:03:03 +0530

    Update welcome.java

--- java/welcome.java
+++ java/welcome.java
@@ -1,10 +1,7 @@
+//create a file welcome.java
 public class MyFirstJavaProgram {
-
-   /* This is my first java program.
-    * This will print 'Hello World' as the output
-    */
-
    public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
+      System.out.println("Hello World"); 
+      // prints Hello World
    }
 }
