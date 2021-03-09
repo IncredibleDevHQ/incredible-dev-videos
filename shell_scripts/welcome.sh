e9d48e7653968154b5830735d3ed12461dd81196
hemalathank-27 <56719334+hemalathank-27@users.noreply.github.com>
2021-03-09 10:56:36 +0530

    Update welcome.sh

--- shell_scripts/welcome.sh
+++ shell_scripts/welcome.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
+#Create a file called welcome.sh($vi welcome.sh)
 greeting="Welcome"
 user=$(whoami)
 day=$(date +%A)
 
-echo "$greeting back $user! Today is $day, which is the best day of the entire week!"
+echo "$greeting back $user! Today is $day, which is the best day !"
 echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
 
-# output
-# Welcome back hemaalatha! Today is Monday, which is the best day of the entire week!
+#For giving permissions $chmod 777 welcome.sh 
+#to execute $./welcome.sh
+# Welcome back Incredibles' ! Today is Monday, which is the best day !
 # Your Bash shell version is: 5.0.17(1)-release. Enjoy!
