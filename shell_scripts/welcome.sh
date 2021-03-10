62125212821a2348e43ecf2de2f00c58da20254c
hemalathank-27 <56719334+hemalathank-27@users.noreply.github.com>
2021-03-10 11:00:20 +0530

    Update welcome.sh

--- shell_scripts/welcome.sh
+++ shell_scripts/welcome.sh
@@ -2,13 +2,9 @@
 #!/bin/bash
 greeting="Welcome"
 user=$(whoami)
-day=$(date +%A)
-
 echo "$greeting back $user!"
-echo "Today is $day, which is the best day !"
 echo "Your Bash shell version is: $BASH_VERSION." 
 
 #to execute $./welcome.sh
 # Welcome back Incredibles' !
-#Today is Monday, which is the best day !
 # Your Bash shell version is: 5.0.17(1)-release. !
