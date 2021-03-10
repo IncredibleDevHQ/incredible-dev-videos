c1ccc8d50b88b380b187b67f3a5bd72f9a0be83f
Ash <ashwin.mirskar@gmail.com>
2021-03-10 13:46:24 +0530

    Update intro.py

--- ElasticSearch_101/intro.py
+++ ElasticSearch_101/intro.py
@@ -1,19 +1,14 @@
-# Elastic Search
-# Super Easy Installation -> $ pip install elastic-apm[flask]
 from flask import Flask
 from elasticapm.contrib.flask import ElasticAPM
 from elasticapm.handlers.logging import LoggingHandler
-
 app = Flask(__name__)
 apm = ElasticAPM(app)
-
 @app.route('/')
 def bar():
     try:
         1 / 0
     except ZeroDivisionError:
         app.logger.error( 'I cannot math', exc_info=True)
-        
 if __name__ == '__main__':
     # Create a logging handler and attach it.
     handler = LoggingHandler(client=apm.client)
