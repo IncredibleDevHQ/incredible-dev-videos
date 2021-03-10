0f19cf0b6b3e266a609aa1bfdfa76ce3e89acafe
hemalathank-27 <56719334+hemalathank-27@users.noreply.github.com>
2021-03-10 11:05:31 +0530

    Update welcome.sh

--- shell_scripts/welcome.sh
+++ shell_scripts/welcome.sh
@@ -1,10 +1,7 @@
-#welcome.sh
 #!/bin/bash
-greeting="Welcome"
+greeting="Hello "
 user=$(whoami)
-echo "$greeting back $user!"
-echo "Your Bash shell version is: $BASH_VERSION." 
+echo "$greeting  $user!" 
 
-#to execute $./welcome.sh
-# Welcome back Incredibles' !
-# Your Bash shell version is: 5.0.17(1)-release. !
+# Hello Incredibles'!
+
