841de16ab4c7cb07e8fd44552c2e4fc42d7d06f9
Shreya Tabjul <57003582+stabjul@users.noreply.github.com>
2021-03-12 17:21:47 +0530

    Update Arrays.java

--- Data Structures 101 in Java/Arrays.java
+++ Data Structures 101 in Java/Arrays.java
@@ -1,16 +1,16 @@
-dataType []arr; (or) dataType arr[];  
+dataType []arr; (or) dataType arr[];
 
-arrayRefVar=new datatype[size];  
+arrayRefVar=new datatype[size];
 
-class Testarray{  
-public static void main(String args[]){  
+class Testarray{
+public static void main(String args[]){
 int num[]=new int[5];  
-num[0]=10;  
-num[1]=20;  
-num[2]=70;  
+num[0]=10;
+num[1]=20;
+num[2]=70;
 
-for(int i=0;i<num.length;i++) 
-  System.out.println(num[i]);  
-}}  
+for(int i=0;i<num.length;i++)
+  System.out.println(num[i]);
+}}
 
 int nums[]={33,3,4,5};
