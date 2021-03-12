4e67e69b4c475b7c096639cfdb85437ee273e24b
Shreya Tabjul <57003582+stabjul@users.noreply.github.com>
2021-03-12 17:03:27 +0530

    Update Arrays.java

--- Data Structures -Java/Arrays.java
+++ Data Structures -Java/Arrays.java
@@ -4,13 +4,13 @@ arrayRefVar=new datatype[size];
 
 class Testarray{  
 public static void main(String args[]){  
-int a[]=new int[5];  
-a[0]=10;  
-a[1]=20;  
-a[2]=70;  
+int num[]=new int[5];  
+num[0]=10;  
+num[1]=20;  
+num[2]=70;  
 
-for(int i=0;i<a.length;i++) 
+for(int i=0;i<num.length;i++) 
   System.out.println(a[i]);  
 }}  
 
-int a[]={33,3,4,5};
+int nums[]={33,3,4,5};
