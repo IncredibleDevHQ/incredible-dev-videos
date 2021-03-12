8ee18b4ab2db1a53f5fedcc693cd5d9acdc83a35
Shreya Tabjul <57003582+stabjul@users.noreply.github.com>
2021-03-12 16:58:31 +0530

    Create Arrays.java

---
+++ Data Structures -Java/Arrays.java
@@ -0,0 +1,14 @@
+dataType []arr; (or) dataType arr[];  
+
+arrayRefVar=new datatype[size];  
+
+class Testarray{  
+public static void main(String args[]){  
+int a[]=new int[5];  
+a[0]=10;  
+a[1]=20;  
+a[2]=70;  
+
+for(int i=0;i<a.length;i++) 
+  System.out.println(a[i]);  
+}}  
