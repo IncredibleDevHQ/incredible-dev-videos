30c086fdafa44e8113f1f7ef29813ced674b33be
Ash <ashwin.mirskar@gmail.com>
2021-03-15 15:20:11 +0530

    initial commit

---
+++ Python101/ep1-intro.py
@@ -0,0 +1,15 @@
+"""
+Programming is like magic.
+You write very specific instructions in arcane languages to invoke commands 
+and if  you get it even a little bit wrong 
+you risk unleasing demons and destroy everything.
+"""
+# Welcome to Python 101
+# Why python ?
+
+# Download Python from : http://python.org
+
+# Inputting from the user
+name = input()
+# Output the name
+print("Hisss! Hello Incredible ",name)
