68f7425fbd73e30a4480c11915501724b13dcd11
hemalathank-27 <56719334+hemalathank-27@users.noreply.github.com>
2021-03-15 13:47:40 +0530

    Update hello.c

--- c/hello.c
+++ c/hello.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 
-int main() {
-   /* my first program in C */
-   printf("Hello, World! \n");
-   return 0;
-}
-#Hello, World!
+
+#include <stdio.h> 
+int main(void) 
+{ 
+    if (printf("Hello World")) { 
+    } 
+} 
