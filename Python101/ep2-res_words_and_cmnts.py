f119d4b45b32adfe63e56f10ad0368e4dc9724fa
Ash <ashwin.mirskar@gmail.com>
2021-03-15 15:20:44 +0530

    Add files via upload

---
+++ Python101/ep2-res_words_and_cmnts.py
@@ -0,0 +1,17 @@
+# Comments and Reserved Words
+name = "Rossum" # Setting value on name variable.
+"""
+False 	True 	if 	try
+None 	del 	import 	return 
+... and the rest of 33 words.
+"""
+# Rules for naming a variable
+Name = "Gabe" # Allowed
+NaMe = "Tyler" # Allowed
+name_2 = "Torvald" #Allowed
+_name = "James Bond" #Allowed
+
+# Following is not allowed
+123Name = "newName"
+*name = "Maine"
+name! = "Ash"
