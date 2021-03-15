5b0077a9775e6903aabd64d0baa71a5445125eaf
Suvij Surya <suvijsurya76@gmail.com>
2021-03-15 15:00:26 +0530

    Update HelloWorld.hs

--- Haskell/HelloWorld.hs
+++ Haskell/HelloWorld.hs
@@ -1,7 +1,2 @@
-main = putStrLn "Hello, World!"
-
---if more than one line of code 
-main = do
-    putStr "Hello,"
-    putStrLn "Incredible developer"
-    return ()
+ main = do
+    putStr "Hello,World!"
