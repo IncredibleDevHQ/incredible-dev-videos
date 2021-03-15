b5e691160baee7900e4eda9c3a97217b7b9379f3
hemalathank-27 <56719334+hemalathank-27@users.noreply.github.com>
2021-03-15 12:52:19 +0530

    Create understandingargs.sh

---
+++ shell_scripts/understandingargs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Call this script with at least 3 parameters
+echo "First parameter is $1"
+echo "Second parameter is $2"
+echo "Third parameter is $3"
+exit 0
+#output: ./file.sh 40 60 70
+#First parameter is 40
+#Second parameter is 60
+#Third parameter is 70
