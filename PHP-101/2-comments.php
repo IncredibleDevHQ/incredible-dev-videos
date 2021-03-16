13da1e4ed5271b4a0d392c909e67a5f58b37bb46
Prashant Shahi <prashant@incredible.dev>
2021-03-16 16:48:57 +0530

    Added comments in PHP

---
+++ PHP-101/2-comments.php
@@ -0,0 +1,24 @@
+<!-- Comments in PHP -->
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+  # Here is a single-line comment
+  
+  // Here is another single-line comment
+
+  /*
+    You can have multiple-lines comment block
+    that spans over
+    multiple lines
+  */
+  
+  // You can also use comments to skip parts of a code in a single line
+  $sum = 5 /* + 15 */ + 5;
+  echo $sum;
+?>
+
+</body>
+</html>
