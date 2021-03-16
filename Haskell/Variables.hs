cfea50ae33cde37360ec0b547613720e8fe4565d
Suvij Surya <suvijsurya76@gmail.com>
2021-03-16 15:44:58 +0530

    Update Variables.hs

--- Haskell/Variables.hs
+++ Haskell/Variables.hs
@@ -1,10 +1,7 @@
 main = do
    let company = "Incredible.dev" --string
    let score = 4  --Int
-   let win = True --Bool
    putStr("Company is:") 
    print(company)
    putStr("Score is:") 
    print(score)
-   putStr("Did they win:") 
-   print(win)
