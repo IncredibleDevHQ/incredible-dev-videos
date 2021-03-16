30c1d2746c23550e3f617501b5742b89905fbe29
Nithin Kumar B <44922285+The-Index@users.noreply.github.com>
2021-03-16 15:06:49 +0530

    Create TypeCasting.dart

---
+++ Flirt with Dart/TypeCasting.dart
@@ -0,0 +1,17 @@
+void main() {
+  // String -> int
+  var one = int.parse('1');
+  print(one == 1); //true
+
+  // String -> double
+  var onePointOne = double.parse('1.1');
+  print(onePointOne == 1.1); //true
+
+  // int -> String
+  String oneAsString = 1.toString();
+  print(oneAsString == '1'); //true
+
+  // double -> String
+  String piAsString = 3.14159.toStringAsFixed(2);
+  print(piAsString == '3.14'); //true
+}
