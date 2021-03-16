002041b492bcbc47cadf6868a4a2a13b64f4670d
Nithin Kumar B <44922285+The-Index@users.noreply.github.com>
2021-03-16 14:44:48 +0530

    Create Variables2.dart

---
+++ Flirt with Dart/Variables2.dart
@@ -0,0 +1,15 @@
+void main() {
+  int? x; // holds null
+  
+  late String name1;
+  //print(name1) throws Error
+  name1 = "Hello";
+  late String name2 = "World";
+  
+  final year = 2021;
+  //final int year = 2021;
+  const pi = 3.14;
+  
+  print("$x $name1 $name2 $year $pi")
+  //null Hello World 2021 3.14
+}
