f52683cc18e78d0bad71bb93f3e90dae4f65aecd
Nithin Kumar B <44922285+The-Index@users.noreply.github.com>
2021-03-16 15:00:03 +0530

    Create NumberBoolString.dart

---
+++ Flirt with Dart/NumberBoolString.dart
@@ -0,0 +1,15 @@
+void main() {
+  int x = 10;
+  double y = 1.4;
+  
+  bool z = true; // or false
+  
+  String s1 = "Hello ";
+  String s2 = "World!";
+  String s3 = s1 + s2;
+   
+  print("$x $y $z");
+  // 10 1.4 true
+  print(s3);
+  // Hello World!
+}
