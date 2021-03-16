31c6e2000e49e0dd40e28739b0b24a5cb761b8c6
Nithin Kumar B <44922285+The-Index@users.noreply.github.com>
2021-03-16 20:25:46 +0530

    Create ControlFlow1.dart

---
+++ Dart-101/ControlFlow1.dart
@@ -0,0 +1,19 @@
+void main() {
+  int a = 2;
+  
+  if(a == 1) {
+    print("1");
+  } 
+  else if(a == 2) {
+    print("2");
+  } 
+  else {
+    print("not 1 or 2");
+  }
+  
+  switch(a) {
+      case 1 : print("1");break;
+      case 2 : print("2");break;
+      default : print("not 1 or 2");break;
+  }
+}
