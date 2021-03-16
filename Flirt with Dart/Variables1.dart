36d7d03bc1f52275678400ca51052e26d43f8ce6
Nithin Kumar B <44922285+The-Index@users.noreply.github.com>
2021-03-16 14:18:23 +0530

    Create Variables1.dart

---
+++ Flirt with Dart/Variables1.dart
@@ -0,0 +1,9 @@
+void main() {
+  var name = 'Incredible.dev';
+  //String name = "Incredible.dev";
+  var year = 2021;
+  //int year = 2021;
+  var pi = 3.14;
+  //double pi = 3.14;
+  print("$name $year $pi");
+}
