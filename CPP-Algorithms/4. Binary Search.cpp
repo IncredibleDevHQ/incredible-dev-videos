6aa8e729d89764d7e97295f2a667a1fa050dbcba
Sachin Jangid <sachinjangid832@gmail.com>
2021-03-17 11:39:34 +0530

    Create 4. Binary Search.cpp

---
+++ CPP-Algorithms/4. Binary Search.cpp
@@ -0,0 +1,17 @@
+int binary_search(int arr[], int n, int key) {
+    int start = 0;
+    int end = n - 1;
+    while(start <= end) {
+        int mid = (start+end)/2;
+        if(arr[mid] == key) {
+            return mid;
+        }
+        else if (arr[mid] > key) {
+            end = mid -1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
