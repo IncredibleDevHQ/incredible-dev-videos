f531dd1bdf9eb289c042500d695567dbcbc2579c
Ash <ashwin.mirskar@gmail.com>
2021-03-17 14:26:15 +0530

    Update ep11-lists.py

--- Python101/ep11-lists.py
+++ Python101/ep11-lists.py
@@ -6,5 +6,9 @@ print(vowel) # Output : 'b'
 vowels = ['a','e','i']
 nums = [1 , "two" , 3 , "four" ]
 
-vowels.append("o")
-print(vowels) # []
+vowels.append("d") # ['a', 'e', 'i', 'd']
+vowels.remove("d") # ['a', 'e', 'i']
+vowels.extend(["o","u"]) # ['a', 'e', 'i', 'o', 'u']
+print( dir(vowels) )
+#  [...,'clear','copy','count','index','insert',
+#   'pop','reverse','sort', ... ]
