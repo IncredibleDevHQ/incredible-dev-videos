1cffd458d01fce7b6facec2e93282d2009612098
Prashant Shahi <prashant@incredible.dev>
2021-03-18 20:19:49 +0530

    Added Simple GraphQL resolver

---
+++ GraphQL-101/2-simple-resolver.js
@@ -0,0 +1,10 @@
+var { graphql, buildSchema } = require('graphql');
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+var root = { hello: () => 'Hello world!' };
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
