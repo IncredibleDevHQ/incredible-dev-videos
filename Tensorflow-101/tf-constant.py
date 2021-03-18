54619cee32ed15bb0ecd3f5ddefe77982b671166
Karthic Rao <kartronics85@gmail.com>
2021-03-18 13:54:05 +0530

    Update tf-constant.py

--- Tensorflow-101/tf-constant.py
+++ Tensorflow-101/tf-constant.py
@@ -1,3 +1,12 @@
 import tensorflow as tf
 msg = tf.constant('Hello, TensorFlow!')
-tf.print(msg)
+print(msg)
+# tf.Tensor(b'Hello, TensorFlow!', shape=(), dtype=string)
+l = tf.constant([1, 2, 3])
+print(l)
+# tf.Tensor([1 2 3], shape=(3,), dtype=int32)
+print(print(tf.constant([1, 2, 3], dtype=tf.float64)))
+# tf.Tensor([1. 2. 3.], shape=(3,), dtype=float64)
+tf.constant(0, shape=(2, 3))
+# <tf.Tensor: shape=(2, 3), dtype=int32, 
+# numpy=array([[0, 0, 0],[0, 0, 0]], dtype=int32)>
