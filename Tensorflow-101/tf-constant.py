ff1cb7285427d3f851fee292ece3b8838728a7f8
hemalathank-27 <56719334+hemalathank-27@users.noreply.github.com>
2021-03-18 15:37:05 +0530

    Update tf-constant.py

--- Tensorflow-101/tf-constant.py
+++ Tensorflow-101/tf-constant.py
@@ -5,7 +5,7 @@ print(msg)
 l = tf.constant([1, 2, 3])
 print(l)
 # tf.Tensor([1 2 3], shape=(3,), dtype=int32)
-print(print(tf.constant([1, 2, 3], dtype=tf.float64)))
+print(tf.constant([1, 2, 3], dtype=tf.float64))
 # tf.Tensor([1. 2. 3.], shape=(3,), dtype=float64)
 tf.constant(0, shape=(2, 3))
 # <tf.Tensor: shape=(2, 3), dtype=int32, 
