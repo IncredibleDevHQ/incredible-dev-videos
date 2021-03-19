79cb7f5d888a9c3c1b18efc5a37c6efb5beb00e9
Ash <ashwin.mirskar@gmail.com>
2021-03-19 16:25:45 +0530

    Update apm-flask-client.py

--- elastic-apm/apm-flask-client.py
+++ elastic-apm/apm-flask-client.py
@@ -1,22 +1,19 @@
 import os
 from flask import Flask
 from elasticapm.contrib.flask import ElasticAPM
-from elasticapm.contrib.flask import ElasticAPM
+from elasticapm.handlers.logging import LogginHandler
 
-flask_app = Flask(__name__)
-flask_app.config['ELASTIC_APM'] = {
-  'SERVICE_NAME': 'Incredible_APM',
-  'SECRET_TOKEN': os.getenv('SECRET_TOKEN'),
-  'SERVER_URL': os.getenv('SERVER_URL'),
-}
-apm_client = ElasticAPM(flask_app,logging=True)
+app = Flask(__name__)
+apm = ElasticAPM(app)
 
-@flask_app.route("/")
-def foo():
+@app.route("/")
+def bar():
     try:
         1 / 0
     except ZeroDivisionError:
-        apm_client.capture_exception()
-
+        app.logger.error('I cannot math',exec_info=True)
+        
 if __name__ == "__main__":
-    flask_app.run(debug=False,port=6060)
+    handler = LoggingHandler(client=apm.client)
+    handler.setLevel(logging.WARM)
+    app.logger.addHandler(handler)
