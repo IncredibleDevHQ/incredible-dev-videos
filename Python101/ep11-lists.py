c8fc862e8481782580ce37fe39609f63c625697c
Pythonista7 <ashwin.mirskar@gmail.com>
2021-03-19 09:07:32 +0530

    adding changes

--- Python101/ep11-lists.py
+++ Python101/ep11-lists.py
@@ -1,10 +1,14 @@
-# Beyond variables 
+# Beyond variables
 vowel = 'a'
 vowel = 'e'
-print(vowel) # Output : 'b'
+print(vowel) # Output : 'e'
 
 vowels = ['a','e','i']
 nums = [1 , "two" , 3 , "four" ]
 
-vowels.append("o")
-print(vowels) # []
+vowels.append("d") # ['a', 'e', 'i', 'd']
+vowels.remove("d") # ['a', 'e', 'i']
+vowels.extend(["o","u"]) # ['a', 'e', 'i', 'o', 'u']
+print( dir(vowels) )
+#  [...,'clear','copy','count','index','insert',
+#   'pop','reverse','sort', ... ]
--- Python101/ep7-Strings-Part1.py
+++ Python101/ep7-Strings-Part1.py
@@ -5,9 +5,6 @@ another_string = 'Welcome '
 dialog = 'He asked , "But sir, python is a snake?!" '
 reply = "Monty replied , 'Of course it is.' " 
 
-multi_line = """ You cans freely type here , without worrying about
-using \n to go on to a new line... """
-
 # Casting!
 txt_num = str(123)
 print(txt_num) # output : '123'
