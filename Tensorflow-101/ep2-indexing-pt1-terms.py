bdb2d5ea8b1c3a4426e338d07e99003a21a4be1c
Pythonista7 <ashwin.mirskar@gmail.com>
2021-03-19 12:57:10 +0530

    updating comments on tensorflow-ep2

--- Tensorflow-101/ep2-indexing-pt1-terms.py
+++ Tensorflow-101/ep2-indexing-pt1-terms.py
@@ -1,14 +1,15 @@
 # Indexing - Shapes terminology
 import tensorflow as tf
 rank_4 = tf.zeros([3, 2, 4, 5])
-# Shape: The length (no of elements) of each of the axes
+# Shape: The length of each of the axes
 print("Shape of tensor:", rank_4.shape) 
 # Shape of tensor: (3, 2, 4, 5)
 # Rank: Number of tensor axes. 
-print("Number of axes:", rank_4.ndim) # Number of axes: 4
-# Axis or Dimension: A particular dimension of a tensor.
+print("Number of axes:", rank_4.ndim) 
+# Number of axes: 4
+# Axis or Dimension
 print("Elements along axis 0 of tensor:", rank_4.shape[0])
 # Elements along axis 0 of tensor: 3
-# Size: The total number of items in the tensor
+# Size: The total number of items 
 print("Total number of elements (3*2*4*5): ", tf.size(rank_4).numpy())
-# Total number of elements (3*2*4*5):  120
+# Total number of elements (3*2*4*5):120
