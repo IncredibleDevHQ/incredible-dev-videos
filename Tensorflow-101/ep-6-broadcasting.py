ddf2e6fa0647c4e2e698b1171907fd6d8841265d
Pythonista7 <ashwin.mirskar@gmail.com>
2021-03-23 13:09:57 +0530

    adding outputs

--- Tensorflow-101/ep-6-broadcasting.py
+++ Tensorflow-101/ep-6-broadcasting.py
@@ -3,13 +3,15 @@ import tensorflow as tf
 x = tf.constant([1, 2, 3])
 y = tf.constant(2)
 z = tf.constant([2, 2, 2])
-# All of these are the same computation
 tf.print(tf.multiply(x, 2)) #[2 4 6]
 tf.print(x * y) #[2 4 6]
 tf.print(x * z) #[2 4 6]
 x = tf.reshape(x,[3,1])
 y = tf.range(1, 5)
 print(tf.multiply(x, y))
+# [[ 1  2  3  4]
+#  [ 2  4  6  8]
+#  [ 3  6  9 12]]
 x_stretch = tf.constant(
     [[1, 1, 1, 1],
     [2, 2, 2, 2],
