38d5b3d85fd21b8ec8043b90312b500da398d9f9
Nithin Kumar B <nithin@incredible.dev>
2021-06-21 20:27:41 +0530

    Added Naming constants good/bad

---
+++ Dart-tips-and-tricks/naming_constants_bad.dart
@@ -0,0 +1,7 @@
+const PI = 3.14;
+const DefaultTimeout = 1000;
+final URL_SCHEME = RegExp('^([a-z]+):');
+
+class Dice {
+  static final NUMBER_GENERATOR = Random();
+}
---
+++ Dart-tips-and-tricks/naming_constants_good.dart
@@ -0,0 +1,7 @@
+const pi = 3.14;
+const defaultTimeout = 1000;
+final urlScheme = RegExp('^([a-z]+):');
+
+class Dice {
+  static final numberGenerator = Random();
+}
